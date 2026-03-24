0da93f915864f3280d60bf6f77b40c891b73eb07
KingaEwaAdamska <kingaewa05@gmail.com>
2026-03-24 20:40:26 +0100

    better finding

--- main.py
+++ main.py
@@ -44,13 +44,8 @@ for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(
-        gray,
-        CHECKERBOARD,
-        cv2.CALIB_CB_ADAPTIVE_THRESH
-        + cv2.CALIB_CB_FAST_CHECK
-        + cv2.CALIB_CB_NORMALIZE_IMAGE
-    )
+
+    ret, corners = cv2.findChessboardCornersSB(gray, CHECKERBOARD)
 
     if ret:
 
